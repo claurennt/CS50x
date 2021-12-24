3fdb0f98ebb69991328cca92b9ec42e2019197e9
claurennt <c.benedetti1989@gmail.com>
2021-12-25 00:06:34 +0100

    organize mario code into multiple files/headers

---
+++ Pset1/get_pyramid_height.h
@@ -0,0 +1,6 @@
+#ifndef GET_PYRAMID_HEIGHT_H
+#define GET_PYRAMID_HEIGHT_H
+
+int get_pyramid_height(void);
+
+#endif
---
+++ Pset1/print_left_right_pyramids.h
@@ -0,0 +1,7 @@
+#ifndef PRINT_LEFT_RIGHT_PYRAMIDS_H
+#define PRINT_LEFT_RIGHT_PYRAMIDS_H
+
+
+void print_left_right_pyramids(int h);
+
+#endif
